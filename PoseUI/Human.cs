76323eaf98ec4ca29761b5ac4397e405b445cab5
Masaki Muto <m.muto0128@gmail.com>
2014-05-01 19:23:53 +0900

    initial

---
+++ PoseUI/Human.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace PoseUI
+{
+	public class Human
+	{
+		struct Body
+		{
+			public float Rotation;
+		}
+
+		struct Head
+		{
+			public Point Center;
+		}
+
+		struct Arm
+		{
+			public Point Tail, Joint, Head;
+		}
+
+		class Joint
+		{
+			Point Position;
+			Joint Parent;
+			List<Joint> Children;
+		}
+
+		Body body;
+		Head head;
+		Arm leftArm, rightArm, leftLeg, rightLeg;
+
+
+
+	}
+}
